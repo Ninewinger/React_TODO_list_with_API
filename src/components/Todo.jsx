33cd0144426d72faaa6f7e3c844cdd485d97daea
Diego Vergara <d.v.petricio@live.com>
2021-09-07 11:14:56 -0300

    terminado

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,27 +1,44 @@
 import React, { useState } from 'react';
 
 function Todo() {
-    const [todo, setTodo] = useState("");
-    const tasks = todo.map((task) =>{
-        return (
-            <>
-            <li>{task}</li><button></button>
-            </>
-            )
-    } )
-    function countItems() {
+    const [todo, setTodo] = useState([]);
+    console.log(todo)
 
+    const lista = todo.map((todo, i) => {
+        return <li className="list" key={i}>{todo}<button className="close" onClick={() => removeTask(i)}>X</button></li>
+    })
+
+    const handleKeypress = e => {
+        if (e.keyCode === 13 && e.target.value !== "") {
+            setTodo(todo.concat(e.target.value))
+            e.target.value = ""
+        }
+    };
+
+    function question() {
+        if (todo.length === 0) {
+            return "No tasks, add a task"
+        }
+        else {
+            return "what need to be done"
+        }
     }
 
+    function removeTask(i) {
+        var array = [...todo];
+        console.log(i)
+        array.splice(i, 1);
+        setTodo(array);
+    }
 
     return (
         <>
             <h1>todos</h1>
-            <div>
-                <input type="text" />
+            <div id="contenedor">
+                <input id="" type="text" placeholder={question()} onKeyUp={handleKeypress} />
                 <ul>
-                    {tasks}
-                    <li>{countItems}</li>
+                    {lista}
+                    <li id="itemsLeft">{todo.length} items left</li>
                 </ul>
             </div>
         </>
