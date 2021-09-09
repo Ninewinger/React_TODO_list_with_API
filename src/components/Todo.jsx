4e4de4d00c289eaa4e7bb24f4ab8923d3513c082
Diego Vergara <d.v.petricio@live.com>
2021-09-08 22:59:40 -0300

    nulls everywhere

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,24 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Todo() {
     const [todo, setTodo] = useState(null);
     console.log(todo)
+    const [url] = "http://localhost:4000/todo";
 
     const lista = todo.map((todo, i) => {
-        return <li className="list" key={i}>{todo}<button className="close" onClick={() => removeTask(i)}>X</button></li>
+        return <li className="list" key={i}>{todo}<button className="close" onClick={() => removeTodo(todo, i)}>X</button></li>
     })
 
     useEffect(() => {
+        getTodo();
+    }, [])
+
+    const getTodo = () => {
         fetch("http://localhost:4000/todo")
-        .then(res => data.json)
-        .then( data => {
-            setTodo(data)
-        },[])
-    })
+            .then(res => res.json)
+            .then(data => {
+                setTodo(data)
+            })
+    }
+
+    const createTodo = (todo) => {
+        fetch("http://localhost:4000/todo", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(todo)
+        })
+            .then(res => res.json)
+            .then(data => {
+                setTodo(data)
+            })
+    }
+
+    const removeTodo = (todo, id) => {
+        fetch(`${url}/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(todo)
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+
+                if (data.id) {
+                    getTodo();
+                }
+            })
+            .catch((error) => console.log(error))
+    }
 
     const handleKeypress = e => {
         if (e.keyCode === 13 && e.target.value !== "") {
-            setTodo(todo.concat(e.target.value))
+            createTodo(e.target.value)
             e.target.value = ""
         }
     };
@@ -32,22 +70,16 @@ function Todo() {
         }
     }
 
-    function removeTask(i) {
-        var array = [...todo];
-        console.log(i)
-        array.splice(i, 1);
-        setTodo(array);
-    }
 
     return (
         <>
             <h1>todos</h1>
             <div id="contenedor">
                 <input id="" type="text" placeholder={question()} onKeyUp={handleKeypress} />
-                <ul>
+                {todo && <ul>
                     {lista}
                     <li id="itemsLeft">{todo.length} items left</li>
-                </ul>
+                </ul>}
             </div>
         </>
     );
